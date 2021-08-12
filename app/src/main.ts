d0854862a21956ff9bdd56c77b7802829597731f
lightcodes845 <dare.falola7@gmail.com>
2021-08-12 20:17:27 +0100

    update

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import {config} from "./config/mongoose";
 
+console.log(config);
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
