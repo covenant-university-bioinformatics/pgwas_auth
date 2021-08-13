5366bac155c11b47ffeb67b17330cf65f5022641
lightcodes845 <dare.falola7@gmail.com>
2021-08-13 05:29:53 +0100

    update

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,15 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import {config} from "./config/mongoose";
 import * as helmet from 'helmet';
-import * as csurf from 'csurf';
-console.log(config);
+import * as mongoSanitize from 'express-mongo-sanitize';
+import * as xss from "xss-clean";
+import * as rateLimit from "express-rate-limit";
+import * as hpp from "hpp";
+
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
-  // app.use(csurf());
+  app.use(mongoSanitize());
+  app.use(xss());
+  const limiter = rateLimit({
+    windowMs: 10*60*1000, //10 mins
+    max: 100
+  })
+  app.use(limiter);
+  app.use(hpp());
   app.use(helmet());
   const port = 3000;
   await app.listen(port);
