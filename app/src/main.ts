01acd697fc43e28440b24afe8dcba6864f767052
lightcodes845 <dare.falola7@gmail.com>
2021-08-13 04:33:44 +0100

    added cors and security

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,12 +3,15 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import {config} from "./config/mongoose";
-
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 console.log(config);
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
+  // app.use(csurf());
+  app.use(helmet());
   const port = 3000;
   await app.listen(port);
 
